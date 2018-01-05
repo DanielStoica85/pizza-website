e5d91878e9cb73ac0269f6e3969f32b2af9f9f46
Daniel <dansto13@yahoo.com>
2018-01-05 23:45:19 +0100

    Add animations.

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
+// Scroll Reveal Animations
+window.sr = ScrollReveal();
+
+sr.reveal(".section-title", {
+    duration: 1000,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    scale: 0.2,
+    mobile: false
+});
+
+sr.reveal(".section-subtitle", {
+    duration: 1000,
+    delay: 1000,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    scale: 0.2,
+    mobile: false
+});
+
+sr.reveal(".left", {
+    duration: 2000,
+    delay: 2000,
+    origin: 'left',
+    distance: '300px',
+    easing: 'ease-in-out',
+    scale: 0.5,
+    mobile: false
+});
+
+sr.reveal(".right", {
+    duration: 2000,
+    delay: 2000,
+    origin: 'right',
+    distance: '300px',
+    easing: 'ease-in-out',
+    scale: 0.5,
+    mobile: false
+});
+
+sr.reveal(".order", {
+    duration: 2000,
+    delay: 2000,
+    scale: 0.2,
+    easing: 'ease-in-out',
+    mobile: false,
+    rotate: { x: 0, y: 360, z: 180}
+}, 300);
+
+sr.reveal(".ingredient-box", {
+    duration: 500,
+    delay: 2000,
+    scale: 0.1,
+    easing: 'ease-in-out',
+    mobile: false,
+    rotate: {x: 0, y: 0, z: 180}
+}, 100);
+
 // Add smooth scrolling to all links
 $("a").on('click', function (event) {
 
